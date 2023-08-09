5543181de499b2331ff2423e02ccafb150ccc4dc
zevelyyn <zevelyn@terpmail.umd.edu>
2023-08-09 10:41:09 -0400

    8/9 commit

--- src/components/Models.jsx
+++ src/components/Models.jsx
@@ -3,67 +3,78 @@ import { OrbitControls } from '@react-three/drei'
 import { Canvas, useLoader } from '@react-three/fiber'
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader"
 import { useSpring, animated, config } from '@react-spring/three'
-import {motion,
-  useScroll,
-  useTransform,
-  MotionValue
-} from "framer-motion"
+import styled from 'styled-components'
 // import { EffectComposer, SSAO, Bloom } from "react-postprocessing"
 
-function Func(id) {
-  const gltf = useLoader(GLTFLoader, '../assets/${id}.gltf')
+const Container = styled.div`
+  height: 60vh;
+  width: 60vh;
+  overflow: hidden;
+  -ms-overflow-style: none;
+  scrollbar-width: none;
+`
+
+const Column = styled.div`
+  height: 60vh;
+  width: 20vh;
+`
+
+function Func({ id }) {
+  const gltf = useLoader(GLTFLoader, `../assets/${id}.gltf`)
+  const [active, setActive] = useState(false)
+    
+  const { scale } = useSpring({ 
+    scale: active ? 1.1 : 1.0,
+    config: config.wobbly
+  })
 
   return (
-    <animated.scene>
+    <animated.scene
+      // scale={scale} 
+      // onPointerEnter={() => setActive(!active)} 
+      // onPointerOut={() => setActive(!active)}
+    >
         <primitive object={gltf.scene} />
     </animated.scene>
   ) 
 }
 
-function Scene() {
-    const gltf = useLoader(GLTFLoader, '../assets/1.gltf')
-    const [active, setActive] = useState(false)
-  
-    const { scale } = useSpring({ 
-      scale: active ? 1.1 : 1.0,
-      config: config.wobbly
-    })
-  
+function Scene({ num }) {
     return (
-      <animated.scene 
-        scale={scale} 
-        onPointerEnter={() => setActive(!active)} 
-        onPointerOut={() => setActive(!active)}
+      <Canvas 
+        shadows 
+        camera={{fov: 25, position: [-2, 2, 8]}}
       >
-        <primitive object={gltf.scene} />
-      </animated.scene>
+        <Suspense fallback={null}>
+          {/* <Scene/> */}
+          {/* {[1, 2].map((x)=> (<Func id={x}/>))} */}
+          <Func id={num}/>
+          <OrbitControls autoRotate={false} enableZoom={false}/>
+          <ambientLight intensity={.5} color={'peachpuff'}/>
+          <directionalLight position={[3, 2, 1]} color={'gold'}/>
+          <directionalLight position={[-3, -2, -1]} color='honeydew'/>
+          <hemisphereLight intensity={1}/>
+          {/* <EffectComposer smaa>
+            <Bloom />
+            <SSAO />
+          </EffectComposer> */}
+        </Suspense>         
+      </Canvas>
     )
 }
 
+
+
 const Models = () => {
-    return(
-      <div className='App'>
-        <Canvas 
-          shadows 
-          camera={{fov: 25, position: [-2, 2, 8]}}
-        >
-          <Suspense fallback={null}>
-            <Scene/>
-            {/* {[1, 2].map((x)=> (<Func id={x}/>))} */}
-            {/* <Func id={1}/> */}
-            <OrbitControls autoRotate={false} enableZoom={false}/>
-            <ambientLight intensity={.5} color={'peachpuff'}/>
-            <directionalLight position={[3, 2, 1]} color={'gold'}/>
-            <directionalLight position={[-3, -2, -1]} color='honeydew'/>
-            <hemisphereLight intensity={1}/>
-            {/* <EffectComposer smaa>
-              <Bloom />
-              <SSAO />
-          </EffectComposer> */}
-          </Suspense>         
-        </Canvas>
-      </div>
-    )
+  return(
+    <Column>
+      <ul>
+        <Container><Scene num={'1'}/> </Container>
+        <Container><Scene num={'2'}/> </Container>
+      </ul>
+      
+    </Column>
+  )
 }
 
 export default Models
